ab0e52518fcf3a049c7a0605d5a199b06049c5f2
Your Name <you@example.com>
2020-07-29 04:42:20 +0700

    up

---
+++ auto_git.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+git add $(pwd)/.
+
+echo -n 'Enter the commit message : '
+read commitMessage
+git commit -m "$commitMessage"
+echo -n 'Enter the name of the branch : '
+read branch
+git push origin $branch
+read
